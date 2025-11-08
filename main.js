79b45f9fdd7463c4ff02d3c18cc1549a35aeb473
Messef <85040583+AkTewari123@users.noreply.github.com>
2025-11-08 16:24:13 -0500

    comitting socket server

---
+++ main.js
@@ -0,0 +1,24 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log("A user connected:", socket.id);
+
+  socket.on("message", (msg) => {
+    console.log("Message received:", msg);
+    io.emit("message", msg); // Broadcast the message to all connected clients
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
